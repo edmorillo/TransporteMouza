07c62aaa6475db6489221014ef473e7c50e5cfd4
edmorillo <edisonmorillo489@gmail.com>
2023-08-28 20:31:32 -0300

    Mejoras

--- TransporteMouza/Models/Compra.cs
+++ TransporteMouza/Models/Compra.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace TransporteMouza.Models
 {
@@ -14,8 +16,12 @@ namespace TransporteMouza.Models
         public string? Neumatico { get; set; }
         public int? Cantidad { get; set; }
         public decimal? Precio { get; set; }
+        [Display(Name = "Detalles")]
         public string? Detalle { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de compra")]
         public DateTime? FechaCompra { get; set; }
+        [Display(Name = "Forma de Pago")]
         public string? FormaDePago { get; set; }
 
         public virtual ICollection<Neumatico> Neumaticos { get; set; }
